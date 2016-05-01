de03c404ce22c9accce4752201f60a7ea8c99012
Roozeppe Jean Pierre <roozeppe.jp@gmail.com>
2016-05-01 14:43:04 -0400

    comments added, don't know how to use dt and player update yet

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
-// Enemies our player must avoid
+// Enemies our player must avoid.
 var Enemy = function(y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+    // Variables applied to each of our instances go here.
+    // Set the coordinates of an enemy to start off canvas.
     this.x = -1000;
     this.y = y;
 
     this.speed = Math.floor((Math.random() * 8) + 5);
 
     // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // a helper we've provided to easily load images.
     this.sprite = 'images/enemy-bug.png';
-    //console.log(this.x, this.y);
 };
 
 // Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Parameter: dt, a time delta between ticks.
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed;
+
     // Enemy goes off canvas, bring it back and randomized its speed.
     if (this.x > 600) {
         this.x = Math.floor((Math.random() * (-1000)) - 500);
@@ -27,38 +27,40 @@ Enemy.prototype.update = function(dt) {
     };
 };
 
+// Check for collisins with player.
 var checkCollisions = function() {
     allEnemies.forEach(function(enemy) {
 
-        if (enemy.x < player.x + 101 &&
-            enemy.x + 101 > player.x &&
-            enemy.y < player.y + 70 &&
-            70 + enemy.y > player.y) {
-                // collision detected!
+        if (enemy.x < player.x + 70 &&
+            enemy.x + 70 > player.x &&
+            enemy.y < player.y + 50 &&
+            50 + enemy.y > player.y) {
+                // Collision detected!
                 player.x = 200;
                 player.y = 390;;
             }
         });
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen, required method for game.
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player Class.
 var Player = function() {
+    // Initial position of a Player's instance.
     this.x = 200;
     this.y = 390;
 
     this.sprite = 'images/char-boy.png';
 };
 
+// Don't know what to do with this yet.
 Player.prototype.update = function(dt) {
 };
 
+// Draw the player on the screen.
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -95,10 +97,10 @@ Player.prototype.handleInput = function(key) {
     };
 };
 
-// Now instantiate your objects.
-var enemie_0 = new Enemy(45), enemie_1 = new Enemy(140), enemie_2 = new Enemy(225), 
-    enemie_3 = new Enemy(45), enemie_4 = new Enemy(140), enemie_5 = new Enemy(225), 
-    enemie_6 = new Enemy(45), enemie_7 = new Enemy(140), enemie_8 = new Enemy(225);
+// Instantiate the enemies.
+var enemie_0 = new Enemy(60), enemie_1 = new Enemy(145), enemie_2 = new Enemy(225), 
+    enemie_3 = new Enemy(60), enemie_4 = new Enemy(145), enemie_5 = new Enemy(225), 
+    enemie_6 = new Enemy(60), enemie_7 = new Enemy(145), enemie_8 = new Enemy(225);
 
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [enemie_0, enemie_1, enemie_2, enemie_3, enemie_4, enemie_5, enemie_6, enemie_7, enemie_8];
